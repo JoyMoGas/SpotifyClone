5960196aba164034e36f34869dee0bca5f2ef3a4
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2026-01-27 10:45:29 -0700

    feat: New navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,28 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight, faSearch, faHome } from '@fortawesome/free-solid-svg-icons';
+import './styles/Navbar.css';
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div style={{ display: 'flex', gap: '10px' }}>
-        <button style={{ borderRadius: '50%', width: '30px', height: '30px', border: 'none', background: '#000', color: 'white' }}><FontAwesomeIcon icon={faChevronLeft} /></button>
-        <button style={{ borderRadius: '50%', width: '30px', height: '30px', border: 'none', background: '#000', color: 'white' }}><FontAwesomeIcon icon={faChevronRight} /></button>
+      <div className="arrows">
+        <button><FontAwesomeIcon icon={faChevronLeft} /></button>
+        <button><FontAwesomeIcon icon={faChevronRight} /></button>
+      </div>
+
+      <div className="home-icon">
+        <FontAwesomeIcon icon={faHome} />
+      </div>
+
+      <div className="search">
+        <FontAwesomeIcon icon={faSearch} />
+        <input type="text" placeholder="¿Qué quieres escuchar?">
+        </input>
       </div>
       
-      <div style={{ marginLeft: 'auto' }}>
-        <button style={{ padding: '8px 16px', borderRadius: '20px', border: 'none', fontWeight: 'bold' }}>
-            Explorar Premium
+      <div className="actions">
+        <button style={{ padding: '8px 16px', width: '40px', height: '40px', borderRadius: '100%', border: 'none', fontWeight: 'bold' }}>
+            
         </button>
       </div>
     </nav>
